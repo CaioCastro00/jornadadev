8ca53670a4070ce0aa39a2fe7807e64f6f7a5afa
Caio Castro <caiocastro@poli.ufrj.br>
2023-06-17 17:53:31 -0300

    Base structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Video from "./pages/Video";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="app__videos">
+        <Video
+          likes={325}
+          messages={206}
+          shares={683}
+          name="Caio_Castro"
+          description="Goalkeeper's test for next catolympics"
+          music="Epic training music"
+          url="https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/brecker2.mp4?t=2023-05-22T19%3A37%3A45.885Z"        
+        />
+        <Video
+          likes={325}
+          messages={206}
+          shares={683}
+          name="Caio_Castro"
+          description="Goalkeeper's test for next catolympics"
+          music="Epic training music"
+          url="https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/bird.mp4?t=2023-05-22T19%3A40%3A47.052Z"        
+        />
+        <Video/>
+        <Video/>
+      </div>
     </div>
   );
 }
